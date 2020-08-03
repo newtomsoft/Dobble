2f202c630dcd8579c6d698c2a0e3146b74b33b02
newtomsoft <thomas@newtomsoft.fr>
2020-08-03 11:36:01 +0200

    Ajoutez des fichiers projet.

---
+++ Dobble/DobbleCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DobbleGenerator
+{
+    public class DobbleCard
+    {
+        public List<int> Values { get; }
+
+
+        public DobbleCard() => Values = new List<int>();
+        public DobbleCard(int firstValue) => Values = new List<int> { firstValue };
+
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var value in Values)
+                stringBuilder.Append(value + "-");
+
+            return stringBuilder.Remove(stringBuilder.Length - 1, 1).ToString();
+        }
+    }
+}
---
+++ Dobble/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DobbleGenerator
+{
+    internal class Program
+    {
+        protected Program()
+        {
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Generation of Dobble cards :");
+            Console.WriteLine();
+            Console.WriteLine(new DobbleGame(6));
+        }
+    }
+}
